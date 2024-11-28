267565d8b7fbdd9f5576af0fd3e4dce3ede5b01c
dwi312 <dwi31287no5@gmail.com>
2024-11-28 20:18:40 +0700

    Deklarasi Variabel

--- dasar java/Dasar.java
+++ dasar java/Dasar.java
@@ -1,6 +1,31 @@
 public class Dasar {
     public static void main(String[] args) {
-        System.out.println("membuat struktur dasar program java");
+        System.out.println("1. membuat struktur dasar program java");
+
+        System.out.println("2. membuat Deklarasi Variabel program java");
+
+        // Tipe Data Primitif
+        int angka = 10;                 // Bilangan bulat
+        double desimal = 3.14;          // Bilangan desimal
+        char huruf = 'A';               // Karakter tunggal
+        boolean status = true;           // Nilai benar/salah
+        String teks = "Hello World";    // Teks/string
+
+        // Deklarasi tanpa inisialisasi
+        int umur;
+        umur = 25;
+
+        // Konstanta
+        final double PI = 3.14159;
+
+        System.out.println("ini adalah Bilangan bulat int : " +angka);
+        System.out.println("ini adalah Bilangan desimal double : " +desimal);
+        System.out.println("ini adalah karater tunggal char : " +huruf);
+        System.out.println("ini adalah nilai benar/salah boolean : " +status);
+        System.out.println("ini adalah text string : " + teks);
+
+        System.out.println("berapa usia budi sekarang ? jawaban " + umur);
+        System.out.println("rumus lingkaran adalah  " + PI);
     }
 
 }
