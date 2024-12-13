1fc8a0f123844587d537799f542ba3442a7a1280
dwi312 <dwi31287no5@gmail.com>
2024-12-13 23:49:41 +0700

    1. Struktur Dasar Program Java

--- dasar/Main.java
+++ dasar/Main.java
@@ -1,9 +1,17 @@
 package dasar;
 
-public class Main {
-    public static void main(String[] args) {
-        
-        System.out.println("Hello world");
+
+/*
+ *  1. Struktur Dasar Program Java :
+ *      a. public class Main: Mendeklarasikan sebuah kelas bernama Main.
+ *      b. public static void main(String[] args): Titik awal eksekusi program.
+ *      c. System.out.println("Hello, World!");: Menampilkan teks ke konsol.
+ */
+
+ public class Main { // public class Main: Mendeklarasikan sebuah kelas bernama Main.
+    public static void main(String[] args) { // public static void main(String[] args): Titik awal eksekusi program.
+
+        System.out.println("Hello world"); // System.out.println("Hello, World!");: Menampilkan teks ke konsol.
     }
 
 }
